75a9068ce1df39543c960027803be5c794d03133
Ayala Shachar <shachar.ayala@gmail.com>
2015-08-23 11:43:37 +0300

    Improve remove_reference's loop

--- reference_counter/reference_counter.py
+++ reference_counter/reference_counter.py
@@ -2,7 +2,7 @@ import sys
 import logbook
 import itertools
 
-from ._compat import reraise, imap
+from ._compat import reraise
 from .exceptions import InvalidReferenceCount
 
 _logger = logbook.Logger(__name__)
@@ -16,7 +16,7 @@ class ReferenceCounter(object):
         self._zero_refcount_callbacks = []
 
     def add_zero_refcount_callback(self, callback):
-        self._zero_refcount_callbacks.append(callback)
+        self._zero_refcount_callbacks.append(lambda: callback(self))
 
     def add_reference(self):
         self._reference += 1
@@ -37,9 +37,9 @@ class ReferenceCounter(object):
             _logger.debug("Last reference dropped for {0!r}", self)
             thrown = None
             for callback in itertools.chain(self._zero_refcount_callbacks,
-                    imap(lambda callback: (lambda refcount: callback.remove_reference()), self._depends_on)):
+                                            (ref_count.remove_reference for ref_count in self._depends_on)):
                 try:
-                    callback(self)
+                    callback()
                 except BaseException:
                     _logger.debug("Exception caught during remove_reference", exc_info=True)
                     if thrown is None:
