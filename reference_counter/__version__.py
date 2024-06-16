f3992fd3515da0dbed45b5d38d596e3a89635af0
Ayala Shachar <shachar.ayala@gmail.com>
2024-06-17 00:24:12 +0300

    Fix __version__

--- reference_counter/__version__.py
+++ reference_counter/__version__.py
@@ -1,1 +1,9 @@
-__version__ = "0.8-dev"
+import sys
+if sys.version_info < (3, 8):
+    import pkg_resources
+    get_distribution = pkg_resources.get_distribution
+else:
+    from importlib.metadata import distribution
+    get_distribution = distribution
+
+__version__ = get_distribution('reference-counter').version
